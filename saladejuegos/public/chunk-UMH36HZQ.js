bd46787cc0735243070cd0c826dead9f1ecedb0c
Fran Diaz B <frandiazb97@gmail.com>
2024-09-27 00:44:20 -0300

    commit

---
+++ saladejuegos/public/chunk-UMH36HZQ.js
@@ -0,0 +1,1 @@
+import{y as c}from"./chunk-DTMHAA3M.js";import{r as o,v as r}from"./chunk-WNPDPR7F.js";var a=class t{authData=r(c);constructor(){}static \u0275fac=function(e){return new(e||t)};static \u0275prov=o({token:t,factory:t.\u0275fac,providedIn:"root"})};export{a};
